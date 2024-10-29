4a0720d3b7bde3b714504be144c0ec1eab625827
negretenico <negretenico0@gmail.com>
2024-10-28 21:25:51 -0400

    Adding ract toast

---
+++ pdp/src/component/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div>
+          <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">  </svg>
+  Loading...
+    </div>
+    )
+}
